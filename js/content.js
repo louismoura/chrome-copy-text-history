cea99002a20fb90f7747db69b1b41a08a4111aa8
nakamura <nakamura@fout.jp>
2021-03-29 21:38:15 +0900

    fix: 履歴に上限(100件)を追加

--- js/content.js
+++ js/content.js
@@ -4,6 +4,9 @@ $(document).bind('copy', function(e){
     let new_history = items.copied_text_list.filter(text => {
       if (text != copied) return true;
     });
+    if (new_history.length >= 100) {
+      new_history = new_history.slice(0, 99);
+    }
     new_history.unshift(copied);
     chrome.storage.local.set({'copied_text_list': new_history}, function(items) {});
   });
